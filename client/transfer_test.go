614f2bd32c97d18a229a273a8d9fb59508bf2d0b
Franco Valencia <franco.avalencia@gmail.com>
2022-03-18 09:33:12 -0600

    Tweaks to transfer test

--- client/transfer_test.go
+++ client/transfer_test.go
@@ -27,11 +27,11 @@ func TestTransferERC20(t *testing.T) {
 	// assert.Nil(t, err)
 
 	// request := &TransferERC20Request{
-	// 	// TokenID:      "6805",
+	// 	TokenID:      "6805",
 	// 	Symbol:       "FCT",
 	// 	TokenAddress: "0x73f99ca65b1a0aef2d4591b1b543d789860851bf",
-	// 	ToAddress:    "0xc0324Dca5073Df1aaf26730471718c500d31cA01",
-	// 	Amount:       "100000000",
+	//  ToAddress:    "0xc0324Dca5073Df1aaf26730471718c500d31cA01",
+	// 	Amount:       "1",
 	// }
 
 	// response, err := client.TransferERC20(request)
@@ -46,7 +46,7 @@ func TestTransferERC721(t *testing.T) {
 	// assert.Nil(t, err)
 
 	// request := &TransferERC721Request{
-	// 	TokenID:      "6805",
+	// 	TokenID:      "1500",
 	// 	TokenAddress: "0x3d25036695dafab7eee3465ff146f6d6c6d0045b",
 	// 	ToAddress:    "0xc0324Dca5073Df1aaf26730471718c500d31cA01",
 	// }
